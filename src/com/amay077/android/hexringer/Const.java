4e476304b5ed678ad61330b2d18e70abc8c45717
amay077 <okuokuoku@gmail.com>
2011-02-12 11:21:17 +0900

    Alarm と通知の調整(no test)
     * Alarm を分刻みにした
     * 通知に前回位置を確認した時刻を表示するようにした

--- src/com/amay077/android/hexringer/Const.java
+++ src/com/amay077/android/hexringer/Const.java
@@ -3,7 +3,9 @@ package com.amay077.android.hexringer;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import com.amay077.android.hexringer.AlarmBroadcastReceiver.LocationUtil;
 import com.amay077.android.logging.Log;
+import com.amay077.android.preference.PreferenceWrapper;
 
 import android.app.AlarmManager;
 import android.app.Notification;
@@ -11,6 +13,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 
 public class Const {
 	// Intent Action
@@ -35,7 +38,7 @@ public class Const {
 	public static final float LOCATION_MAX_ACCURACY = 1000;
 
 	/** AlarmManager にインテント発行を設定する（今からｎ分後） */
-	static public void setNextAlarm(Context context, int delay) {
+	static public void setNextAlarm(Context context, int delay, boolean truncateMili) {
 		try {
 			Intent intent = new Intent(context,
 					AlarmBroadcastReceiver.class);
@@ -52,7 +55,10 @@ public class Const {
 			Calendar cal = Calendar.getInstance();
 			cal.setTimeInMillis(System.currentTimeMillis());
 			cal.add(Calendar.MINUTE, delay);
-			alarmManager.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), sender);
+			final long MINUTE = 1000 * 60;
+			alarmManager.set(AlarmManager.RTC_WAKEUP,
+					truncateMili ? (long)(cal.getTimeInMillis() / MINUTE * MINUTE) : cal.getTimeInMillis(),
+					sender);
 
 			NotificationManager notificationManager =
 				(NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -63,9 +69,13 @@ public class Const {
 					System.currentTimeMillis());
 			Intent notifyIntent = new Intent(context, MainActivity.class);
 			//intentの設定
+			PreferenceWrapper pref = new PreferenceWrapper(context);
+			String lastLocaString = pref.getString(R.string.pref_last_location_key, "");
+			Location lastLocation = LocationUtil.fromString(lastLocaString);
+			String prev = lastLocation != null ? new SimpleDateFormat("H時mm分ss秒").format(lastLocation.getTime()) + " に位置を確認しました。\n" : "";
 			PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notifyIntent, 0);
 			notification.setLatestEventInfo(context.getApplicationContext(),
-					"HexRinger", "次回は " + new SimpleDateFormat("H時mm分ss秒").format(cal.getTime()) + " に確認します", contentIntent);
+					"HexRinger", prev + "次回は " + new SimpleDateFormat("H時mm分ss秒").format(cal.getTime()) + " に確認します", contentIntent);
 			//notification.flags = Notification.FLAG_AUTO_CANCEL;
 			notificationManager.notify(R.string.app_name, notification);
 
